daae993c63b74e8c3615a5f781841a008431a810
cencije <cencije@lafayette.edu>
2018-02-04 15:56:37 -0500

    The basic bash with the code from the book.

---
+++ src/fork.c
@@ -0,0 +1,18 @@
+
+
+int main()
+
+    pid_t pid;
+    int x = 1;
+
+    pid = Fork();
+    if (pid == 0) { /* Child */
+        pritnf("child : x=%d\n", ++x);
+        exit(0);
+    }
+
+    /* Parent */
+    printf("parent: x=%d\n", --x);
+    exit(0);
+
+}
